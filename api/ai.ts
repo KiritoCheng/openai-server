28dcef9f2212d1fea41df7b7653500b3aaf93334
Kirito <1345036885@qq.com>
2023-03-16 16:41:12 +0800

    add

--- api/ai.ts
+++ api/ai.ts
@@ -13,65 +13,23 @@ export const routes = (app: any) => {
       });
 
       const openai = new OpenAIApi(configuration);
-      const response = await openai.createChatCompletion({
+      const response: any = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content }],
         max_tokens: 255,
         temperature: 0.5,
       });
 
-      // 打印 API 返回的结果
-      console.log(response);
-
-      // try {
-      //   const config = {
-      //     url: "https://api.openai.com/v1/chat/completions",
-      //     method: "POST",
-      //     headers: {
-      //       "Content-Type": "application/json",
-      //       Authorization: `Bearer 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`,
-      //     },
-      //     data: {
-      //       model: "gpt-3.5-turbo",
-      //       messages: [{ role: "user", content }],
-      //     },
-      //   };
-
-      //   axios(config)
-      //     .then((response) => {
-      //       console.log("返参", response);
-      //       if (response.data) {
-      //         const { message } = response.data.choices[0];
-      //         const { content } = message || {};
-      //         return res.status(200).json(content);
-      //       }
-      //     })
-      //     .catch((err) => {
-      //       console.log("返参", err);
-      //       const { message = "" } = err || {};
-      //       return res.status(404).json({ message });
-      //     });
-
-      //   // const completion = await openai.createChatCompletion({
-      //   //   model: "gpt-3.5-turbo",
-      //   //   messages: [{ role: "user", content: "test" }],
-      //   // });
-
-      //   // // console.log("返参", completion.data);
-
-      //   // console.log("返参", completion.data);
-      //   // if (completion.data) {
-      //   //   const { message } = completion.data.choices[0];
-      //   //   const { content } = message || {};
-      //   //   return res.status(200).json(content);
-      //   // }
-
-      //   // return res.status(400).json("Error");
-      // } catch (err: any) {
-      //   console.log("错误", err);
-      //   const { message = "" } = err || {};
-      //   return res.status(404).json({ message });
-      // }
+      try {
+        const { text } = response.data.choices[0];
+        // 打印 API 返回的结果
+        console.log(response.data.choices[0]);
+        return res.status(200).json(text);
+      } catch (err: any) {
+        console.log("错误", err);
+        const { message = "" } = err || {};
+        return res.status(404).json({ message });
+      }
     }),
   };
 };
