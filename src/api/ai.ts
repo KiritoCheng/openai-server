1f0b7f1dfe951b0d5449df02da77a3afd7c0e54b
Kirito <1345036885@qq.com>
2023-03-17 11:14:33 +0800

    add

--- src/api/ai.ts
+++ src/api/ai.ts
@@ -3,23 +3,23 @@ import { Configuration, OpenAIApi } from "openai";
 
 export const routes = (app: any) => {
   return {
-    chartOld: app.use("/api/chartOld", async (req: any, res: any) => {
-      const { content = "", OPENAI_API_KEY = "" } = req.query || {};
-
-      const configuration = new Configuration({
-        organization: "org-5ZqRIjonyON5xbje0fcE7zBh",
-        apiKey: OPENAI_API_KEY,
-      });
-
-      const openai = new OpenAIApi(configuration);
-      const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content }],
-        max_tokens: 4097,
-        temperature: 0.5,
-      });
-
+    chart: app.use("/api/chart", async (req: any, res: any) => {
       try {
+        const { content = "", OPENAI_API_KEY = "" } = req.query || {};
+
+        const configuration = new Configuration({
+          organization: "org-5ZqRIjonyON5xbje0fcE7zBh",
+          apiKey: OPENAI_API_KEY,
+        });
+
+        const openai = new OpenAIApi(configuration);
+        const response = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content }],
+          max_tokens: 4097,
+          temperature: 0.5,
+        });
+
         const { message } = response.data.choices[0];
         // 打印 API 返回的结果
         return res.status(200).json(message);
