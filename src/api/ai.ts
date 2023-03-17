5963e8d54a8758049539d75d77d287d18f45387f
Kirito <1345036885@qq.com>
2023-03-17 11:43:54 +0800

    Update ai.ts

--- src/api/ai.ts
+++ src/api/ai.ts
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 export const routes = (app: any) => {
   return {
-    chart: app.use("/api/chart", async (req: any, res: any) => {
+    "chart3.5": app.use("/api/chart3.5", async (req: any, res: any) => {
       try {
         const { content = "", OPENAI_API_KEY = "" } = req.query || {};
 
@@ -26,36 +26,33 @@ export const routes = (app: any) => {
         return res.status(404).json({ message });
       }
     }),
+    chart: app.use("/api/chart", async (req: any, res: any) => {
+      try {
+        const { content = "", OPENAI_API_KEY = "" } = req.query || {};
+        const configuration = new Configuration({
+          organization: "org-5ZqRIjonyON5xbje0fcE7zBh",
+          apiKey: OPENAI_API_KEY,
+        });
+        const openai = new OpenAIApi(configuration);
+
+        const model_engine = "text-davinci-002-render-sha";
+
+        const response = await openai.createCompletion({
+          model: model_engine,
+          prompt: content,
+          n: 1,
+          stop: "\n",
+          temperature: 0.7,
+        });
+        const { text } = response.data.choices[0];
+        // 打印 API 返回的结果
+        console.log(response.data.choices[0]);
+        return res.status(200).json(text);
+      } catch (err: any) {
+        console.log("错误", err);
+        const { message = "" } = err || {};
+        return res.status(404).json({ message });
+      }
+    }),
   };
 };
-
-// chart: app.use("/api/chart", async (req: any, res: any) => {
-//   const { content = "", OPENAI_API_KEY = "" } = req.query || {};
-//   const configuration = new Configuration({
-//     organization: "org-5ZqRIjonyON5xbje0fcE7zBh",
-//     apiKey: OPENAI_API_KEY,
-//   });
-//   const openai = new OpenAIApi(configuration);
-
-//   const model_engine = "text-davinci-002-render-sha";
-
-//   const response = await openai.createCompletion({
-//     model: model_engine,
-//     prompt: content,
-//     max_tokens: 4097,
-//     n: 1,
-//     stop: "\n",
-//     temperature: 0.7,
-//   });
-
-//   try {
-//     const { text } = response.data.choices[0];
-//     // 打印 API 返回的结果
-//     console.log(response.data.choices[0]);
-//     return res.status(200).json(text);
-//   } catch (err: any) {
-//     console.log("错误", err);
-//     const { message = "" } = err || {};
-//     return res.status(404).json({ message });
-//   }
-// }),
