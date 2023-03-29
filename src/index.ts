e3abea597767d55408b465d53ab96abe501b6e7c
Kirito <1345036885@qq.com>
2023-03-29 16:31:11 +0800

    add https

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import { routes } from "./api/ai";
 import { jsonResponse } from "./middlewares/jsonResponse";
+import https from "https";
+import fs from "fs";
 
 const app: express.Application = express();
 
@@ -26,6 +28,12 @@ app.all("*", function (req, res, next) {
 
 routes(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}/`);
+// https 配置
+const httpsOption = {
+  key: fs.readFileSync("/path/ssl/kiritosa.com/key.pem"),
+  cert: fs.readFileSync("/path/ssl/kiritosa.com/fullchain.pem"),
+};
+
+https.createServer(httpsOption, app).listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
 });
