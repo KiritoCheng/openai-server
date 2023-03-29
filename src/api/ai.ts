6a6e57b5927c82058faf6f3bddb0767b251f1b11
Kirito <1345036885@qq.com>
2023-03-29 16:59:16 +0800

    Update ai.ts

--- src/api/ai.ts
+++ src/api/ai.ts
@@ -19,7 +19,7 @@ export const routes = (app: any) => {
         const openai = new OpenAIApi(configuration);
         const response = await openai.createChatCompletion({
           // model: "gpt-3.5-turbo",
-          model: "davinci",
+          model: "text-davinci-002",
           messages: [{ role: "user", content }],
           temperature: 0.7,
         });
